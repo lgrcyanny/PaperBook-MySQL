1fc47b3108f9e56aba1bbe5d9185dcdf034b85b6
Aaron Feng <fengyibin713@gmail.com>
2013-12-20 16:04:22 +0800

    results页面增加分页功能

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -11,24 +11,24 @@ exports.showSearchResults = function(req, res) {
     title = req.session.query;
   }
   var page = req.query.p ? parseInt(req.query.p) : 1;
-  console.log('results_page:' + page);
   Search.findByTitle(title, function(results) {
     var total = results.length;
 
     req.session.query = title;
     var limitedResults = [];
+    var first = page * 10 - 10;
     var length = total - page * 10 + 10;
     if (length > 10) {
       length = 10;
     };
-    console.log('show results form ' + (page * 10 - 10) + "-" + (page * 10 - 11 + length));
-    for (var i = page * 10 - 10; i < page * 10 - 10 + length; i++) {
+    console.log('results_page_' + page+':show results [' + first + "," + (first + length) + ")");
+    for (var i = first; i < first + length; i++) {
       limitedResults.push(results[i]);
     }
     res.render('search/results', {
       title: 'results:',
       page: page,
-      total: Math.floor(total/10)+1,
+      total: Math.floor(total / 10) + 1,
       isFirstPage: page == 1,
       isLastPage: (page * 10) >= total && ((page - 1) * 10) < total,
       results: limitedResults
