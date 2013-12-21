39c76b16ecea6a37bfbca4c6a56abc523d86d720
Aaron Feng <fengyibin713@gmail.com>
2013-12-21 19:17:36 +0800

    simple literature search by title

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,23 +1,22 @@
 //Literature.plugin(textSearch);
 var Literature = require('../models/literature');
-var querystring = require('querystring');
-var url = require('url');
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   res.render("search/index", {});
 };
 
-exports.showSearchResults = function(req, res) {
+exports.showSearchResults = function (req, res) {
 
-  var title = querystring.parse(url.parse(req.url).query)["query"],
-    startTime = new Date().getTime(),
+  var title = req.query.query,
+    startTime = new Date()
+      .getTime(),
     page = req.query.p ? parseInt(req.query.p) : 1,
-    pageSize=10;
+    pageSize = 10;
 
   title = (title === undefined) ? req.session.query : title;
 
   if (page == 1) {
-    Literature.findAll(title, function(err, results) {
+    Literature.findAll(title, function (err, results) {
       if (err) {
         results = []
       };
@@ -26,8 +25,9 @@ exports.showSearchResults = function(req, res) {
     });
   };
 
-  Literature.findByTitle(title, page, pageSize, function(err, results) {
-    var endTime = new Date().getTime(),
+  Literature.findByTitle(title, page, pageSize, function (err, results) {
+    var endTime = new Date()
+      .getTime(),
       total = req.session.total;
 
     if (err) {
