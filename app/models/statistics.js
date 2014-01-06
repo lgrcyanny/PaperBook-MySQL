4a60bb4d4f267a5f6c150baa8b52cf4bef60021a
Aaron Feng <fengyibin713@gmail.com>
2014-01-06 14:19:56 +0800

    brief comments statistics

--- app/models/statistics.js
+++ app/models/statistics.js
@@ -74,7 +74,7 @@ module.exports = {
       .order('add_at');
     console.log(sql.toString());
     utils.exec(sql.toString(), null, function (err, results) {
-      if (err) {
+      if (err || results.length == 0) {
         results = [];
       };
 
@@ -108,6 +108,88 @@ module.exports = {
       });
       // console.log(highstockData);
       callback(highstockData);
+    });
+  },
+
+  getBriefCommentsData: function (req, callback) {
+    var sql = squel.select()
+      .field('created_at')
+      .from('paperbook.brief_comments')
+      .where('user_id=' + req.user.id)
+      .order('created_at');
+    console.log(sql.toString());
+    utils.exec(sql.toString(), null, function (err, results) {
+      if (err || results.length == 0) {
+        results = [];
+      };
+
+      var highstockData = [],
+        count = 0;
+
+      results.forEach(function (result, index) {
+        highstockData.push([new Date(result.created_at)
+          .getTime(), index + 1
+        ]);
+      });
+      // console.log(highstockData);
+     
+      var sql = squel.select()
+        .field('b.add_year', 'add_year')
+        .field('b.username', 'username')
+        .field('ifnull(a.brief_comments_count,0)', 'brief_comments_count')
+        .from(
+          squel.select()
+          .field('year(a.created_at)', 'add_year')
+          .field('b.username', 'username')
+          .field('count(*)', 'brief_comments_count')
+          .from('paperbook.brief_comments', 'a')
+          .join('paperbook.users', 'b', 'a.user_id = b.id')
+          .group('add_year')
+          .group('a.user_id')
+          .group('b.username'), 'a'
+      )
+        .right_join(
+          squel.select()
+          .field('distinct year(a.created_at)', 'add_year')
+          .field('username', 'username')
+          .from('paperbook.brief_comments', 'a')
+          .from('paperbook.users', 'b'), 'b', 'a.add_year=b.add_year and a.username=b.username'
+      )
+        .order('add_year')
+        .order('username');
+      console.log(sql.toString());
+      var highchartsData = Array();
+      utils.exec(sql.toString(), null, function (err, results) {
+        if (err) {
+          results = [];
+        };
+        while (results[0].add_year === null) {
+          results.shift();
+        }
+        var year = results[0].add_year,
+          index = 0,
+          temp = [];
+        results.forEach(function (result) {
+          var result_year = result.add_year;
+          if (year != result_year) {
+            highchartsData.push({
+              'index': index,
+              'name': 'Year ' + year,
+              'data': temp
+            });
+            year = result_year;
+            temp = [];
+          };
+          temp.push([result.username, result.brief_comments_count]);
+        });
+        highchartsData.push({
+          'index': index,
+          'name': 'Year ' + year,
+          'data': temp
+        });
+        callback(highchartsData,highstockData);
+      });
+
     });
   }
 }
--- public/js/statistics-brief-comments.js
+++ public/js/statistics-brief-comments.js
@@ -3,13 +3,37 @@ $(function () {
   var rootURL = 'http://' + window.location.hostname + ':' + window.location.port;
 
   $.ajax({
-    url: rootURL + '/statistics/userViewData',
+    url: rootURL + '/statistics/brief-comments-data',
     type: 'GET',
     data: {},
     success: function (res) {
       if (res.success) {
-        console.log(res.highstockData);
-        $('div#userView')
+
+        $('div#globalView')
+          .highcharts({
+            chart: {
+              type: 'column',
+              inverted: true
+            },
+            title: {
+              text: 'Global View'
+            },
+            credits: {
+              enabled: false
+            },
+            yAxis: {
+              allowDecimals: false,
+              title: {
+                text: 'Articles'
+              }
+            },
+            xAxis: {
+              type: 'category'
+            },
+            series: res.highchartsData
+          });
+
+          $('div#userView')
           .highcharts('StockChart', {
             rangeSelector: {
               selected: 4
@@ -28,7 +52,7 @@ $(function () {
               allowDecimals: false
             },
             series: [{
-              name: 'Literatures',
+              name: 'Brief Commets',
               marker: {
                 enabled: true,
                 radius: 3
@@ -40,7 +64,9 @@ $(function () {
               data: res.highstockData
             }]
           });
+
       }
     }
   });
+
 })
